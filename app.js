afb2192c24051d49cb1e9e27048644d22eb125c4
Mike Zweifel <mike@mikezweifel.ch>
2020-08-10 22:16:43 +0200

    de-colonized session-cookie

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
   store: sessionStore,
   cookie: { maxAge: 600000 },
   resave: false,
-  rolling: true;
+  rolling: true,
   saveUninitialized: false
 }))
 app.use(passport.initialize());
